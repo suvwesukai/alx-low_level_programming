f36a5317d0dd4e2e756730e0ef71fe677037de41
suvwesukai <suvweadidi@gamil.com>
2023-02-19 13:42:16 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * program that prints all single digit numbers of
+ * base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
