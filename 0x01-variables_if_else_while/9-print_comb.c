3f217aca8979bd7ce5aca477714f1a007467c81b
suvwesukai <suvweadidi@gamil.com>
2023-02-20 01:47:01 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Program that prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(',' ' ');
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
