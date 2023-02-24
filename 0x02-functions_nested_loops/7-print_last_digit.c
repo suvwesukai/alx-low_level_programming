2f7b8e67404df92ccccc150401d1e637d8dc3cb0
suvwesukai <suvweadidi@gamil.com>
2023-02-24 09:13:04 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @n: number to be checked
+ *
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+	if (n < 0)
+	{
+		n = -n;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
