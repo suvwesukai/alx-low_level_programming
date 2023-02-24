d1da4148b33af9d47e1e4751942545339cce5589
suvwesukai <suvweadidi@gamil.com>
2023-02-24 10:00:28 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
-	if (n < 0)
+	if (last_digit  < 0)
 	{
-		n = -1 * n;
+		last_digit = -1 * last_digit;
 	}
 	_putchar(last_digit + '0');
 	return (last_digit);
