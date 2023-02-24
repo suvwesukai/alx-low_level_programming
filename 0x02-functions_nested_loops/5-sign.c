4549375e5015e6e39cfe5c5e78c82179b868d971
suvwesukai <suvweadidi@gamil.com>
2023-02-24 07:49:51 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: number to be checked
+ *
+ * Return: 1 and print + if n is greater than 0,
+ * -1 if n is less than 0 and 0 if otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n <= 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
