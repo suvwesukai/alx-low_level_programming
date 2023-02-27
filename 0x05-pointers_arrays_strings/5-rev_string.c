b0ee77fe6beaa3223cfda4c123fab2192d97ed85
suvwesukai <suvweadidi@gamil.com>
2023-02-27 06:30:37 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "mainn.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	int index;
+	char temp;
+
+	len = 0;
+	index = 0;
+	while (s[index++])
+		len++;
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		temp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = temp;
+	}
+}
