da1e3192aa84205bd2981eac981c9c9c9c95a971
suvwesukai <suvweadidi@gmail.com>
2023-03-08 13:41:21 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,14 +9,19 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
+	if (n < 2)
 		return (0);
-	for (i= 2; i < n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+	if (n == 3)
+		return (1);
+	if (n % 3 == 0)
+		return (0);
+	if (n == 5 || n == 7 || n == 11 ||n == 13 || n == 17 || n == 19)
+		return (1);
+	if (n % 5 == 0 || n % 7 == 0 || n % 11 == 0 || n % 13 == 0 || n % 17 == 0 || n % 19 == 0)
+		return (0);
 	return (1);
 }
