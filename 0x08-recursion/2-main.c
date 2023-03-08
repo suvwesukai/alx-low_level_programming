08aed7195761176da1dfa4c829b0dc844fdeb5c2
suvwesukai <suvweadidi@gmail.com>
2023-03-08 08:53:23 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	int  n;
+
+	n = _strlen_recursion("Corin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
