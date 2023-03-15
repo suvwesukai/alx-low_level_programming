03e16c3fd564563f2186c86b5df1beae9d50e3f6
suvwesukai <suvweadidi@gmail.com>
2023-03-15 13:38:32 -0700

    update files

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 0; ((i < 5) && (num > 0)); i++)
 	{
-		cent = cent + (num/coin[i]);
+		cent = cent + (num / coin[i]);
 		num %= coin[i];
 	}
 	printf("%d\n", cent);
