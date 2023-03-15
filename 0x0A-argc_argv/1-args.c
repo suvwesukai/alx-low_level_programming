2beac51ba764197c2a8b9719decaef092a77761c
suvwesukai <suvweadidi@gmail.com>
2023-03-15 09:25:13 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,20 @@
  * Return: Always 0.
  */
 
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
+	/* suppress unused parameter warning for argv */
+	(void)argv;
+
+	if (argc == 1)
+	{
+		argc = 0;
+	}
+	else
+	{
+		argc--;
+	}
+
 	printf("%d\n", argc);
 
 	return (0);
