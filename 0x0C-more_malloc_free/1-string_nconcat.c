e0b4daaa11e19d470942c6572947da92e63f117e
suvwesukai <suvweadidi@gmail.com>
2023-03-16 14:57:38 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,20 +19,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	/* check if strings are empty */
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	/* get length of strings */
 	len1 = 0;
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 	{
 		if (len2 >= n)
@@ -49,18 +43,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	concat_nstring = malloc(sizeof(char) * total_len);
 	if (concat_nstring == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		concat_nstring[i] = s1[i];
-	}
 	for (j = 0; j < len2; j++)
-	{
 		concat_nstring[i + j] = s2[j];
-	}
+
 	concat_nstring[i + j] = '\0';
 
 	return (concat_nstring);
