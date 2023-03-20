fc6afbcce355840666e400e8af385e9a96e76edc
suvwesukai <suvweadidi@gmail.com>
2023-03-20 16:43:52 -0700

    completed task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,10 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - Dog info
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
