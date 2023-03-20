1dcb6d000460948a8cd22bdda4cf3b2197f6d3d8
suvwesukai <suvweadidi@gmail.com>
2023-03-20 11:44:23 -0700

    completed task_4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - Compute the sum of two number
+ * @x: first number
+ * @y: second number
+ *
+ * Return: sum of two numbers
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
