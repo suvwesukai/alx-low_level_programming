d46396ed6de9eb99f4963d151d9b385fe26a7881
suvwesukai <suvweadidi@gmail.com>
2023-03-20 12:52:32 -0700

    completed task_3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,10 +2,11 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /**
+ * File: 3-function_like_macro.h
  * ABS - Compute the absolute value of a number
  * @x: number whose absolute value is to be computed
  */
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) (((x) < (0)) ? (-x) : (x))
 
 #endif /* FUNCTION_LIKE_MACRO_H */
