2d2c8fa8c701f94850bee53cbb584ba2f6e017bc
suvwesukai <suvweadidi@gmail.com>
2023-03-21 12:20:35 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 {
 	int i, j, result;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
 			if (result > 9)
@@ -23,6 +23,10 @@ void times_table(void)
 			}
 			else
 			{
+				if (!(j == 0))
+				{
+					_putchar(' ');
+				}
 				_putchar(result + '0');
 			}
 			if (!(j == 9))
