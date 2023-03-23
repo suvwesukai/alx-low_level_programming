ada443eb8cb2255ab9964a1697c07242929f72bf
suvwesukai <suvweadidi@gmail.com>
2023-03-23 09:53:29 -0700

    task_0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum;
-
 	va_list num;
+	unsigned int i, sum;
 
 	va_start(num, n);
 
 	sum = 0;
-
-	if (n == 0)
-		return (0);
 
 	for (i = 0; i < n; i++)
 	{
