25509f392eb21a9dcb2d9051b824c7c6819f23cc
suvwesukai <suvweadidi@gmail.com>
2023-03-23 09:50:07 -0700

    task_0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
