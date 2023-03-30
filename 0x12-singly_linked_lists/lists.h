65f4020cd3a54cf14c0a24d27a2c93dd3fd164bc
suvwesukai <suvweadidi@gmail.com>
2023-03-30 12:38:51 -0700

    fix bugs

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -23,4 +23,4 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
-#endif /* LIST_H */
+#endif /* LISTS_H */
