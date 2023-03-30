a5aa09e0bcd95d8290d375cdddbc3236f66155ab
suvwesukai <suvweadidi@gmail.com>
2023-03-30 11:47:00 -0700

    print all elements of a list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc' ed string)
+ * @len: length of the string
+ * @next: pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/** FUNCTION POINTERS */
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif /* LIST_H */
