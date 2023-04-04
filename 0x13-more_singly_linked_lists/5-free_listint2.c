9a1861ac2cea923b1b87ac274de04a80b6b28b5a
suvwesukai <suvweadidi@gmail.com>
2023-04-03 18:30:57 -0700

    free linked nodes

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the head of list
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *new_node;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		new_node = temp->next;
+		free(temp);
+		temp = new_node;
+	}
+	*head = NULL;
+}
