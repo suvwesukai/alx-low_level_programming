e054dc90226e4f8c83ba1c3a51187ee8eb2cf9f7
suvwesukai <suvweadidi@gmail.com>
2023-04-04 09:16:21 -0700

    free linked nodes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,18 +8,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *new_node;
+	listint_t *temp;
 
 	if (*head == NULL)
 		return;
 
-	temp = *head;
-	while (temp != NULL)
+	while (*head)
 	{
-		new_node = temp->next;
-		free(temp);
-		temp = new_node;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	*head = NULL;
-	free(*head);
 }
