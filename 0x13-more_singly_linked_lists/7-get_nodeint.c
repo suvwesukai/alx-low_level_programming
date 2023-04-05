29afede442cf2c6958ef42a7f69e3c31f4b46c63
suvwesukai <suvweadidi@gmail.com>
2023-04-05 10:14:13 -0700

    fixed error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *value;
 	unsigned int count = 0;
+	index = 0;
 
 	if (head == NULL || index <= 0)
 		return (NULL);
